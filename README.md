37ea500a179d4ae87a72604ab8ea31c4ee706653
vhavhugo <vhavhugo@gmail.com>
2021-03-28 19:15:03 -0300

    instalando e configurando servidor node

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # nodejs
 Curso nodejs
+
+
+# iniciar novo projeto
+npm init
+
+# instalar a dependecia 
+npm install nodemon
+
+# configurando servidor
+  "scripts": {
+    "start": "node src/index.js",
+    "dev": "nodemon src/index.js"
+  },
--- hello_world/src/index.js
+++ hello_world/src/index.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function(request, response){
     response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("<html><body>Hola Mundo</body></html>");
+    response.write("<html><head> <meta charset=\"utf-8\" /> </head> <body>Holá Mundo</body></html>");
     response.end();
 });
 

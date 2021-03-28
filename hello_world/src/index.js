50a1367bd01f404b4be6a5db38628cfe33c7d040
vhavhugo <vhavhugo@gmail.com>
2021-03-28 20:39:03 -0300

    constantes

--- hello_world/src/index.js
+++ hello_world/src/index.js
@@ -1,8 +1,11 @@
-var http = require('http');
+const http = require('http');
 //var operation = require("./utils/operatons");
-var { multiplication } = require("./utils/operatons");
+const { multiplication } = require("./utils/operatons");
 
-var server = http.createServer((request, response) => {
+const PORT = 5000;
+const apikey = "assasassassasas";
+
+const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write("<html><head> <meta charset=\"utf-8\" /> </head> <body>Holá Mundo</body></html>");
     response.end();
@@ -11,4 +14,4 @@ var server = http.createServer((request, response) => {
 //console.log("multiplication", operation.multiplication(3,5));
 console.log("multiplication", multiplication(3,5));
 
-server.listen(5000);
+server.listen(PORT);

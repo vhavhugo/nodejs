6482d2955333cf22e49dec90e6790f07ca35dc90
vhavhugo <vhavhugo@gmail.com>
2021-03-28 20:50:39 -0300

    rota com if

--- hello_world/src/index.js
+++ hello_world/src/index.js
@@ -6,8 +6,16 @@ const PORT = 5000;
 const apikey = "assasassassasas";
 
 const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("<html><head> <meta charset=\"utf-8\" /> </head> <body>Holá Mundo</body></html>");
+    if(request.url === "/"){
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write("<html><head> <meta charset=\"utf-8\" /> </head> <body>Home</body></html>");
+    }else if(request.url === "/info"){
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write("<html><head> <meta charset=\"utf-8\" /> </head> <body>Info</body></html>");
+    }else{
+        response.writeHead(404, {"Content-Type": "text/html"});
+        response.write("<html><head> <meta charset=\"utf-8\" /> </head> <body>Not found</body></html>");
+    }
     response.end();
 });
 //console.log("addition", operation.addition(3,5));

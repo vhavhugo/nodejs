b90dab5ea50cd32290f8d393094f5a62aa9ec34e
vhavhugo <vhavhugo@gmail.com>
2021-03-28 20:18:41 -0300

    importação parcial e total

--- hello_world/src/index.js
+++ hello_world/src/index.js
@@ -1,12 +1,14 @@
 var http = require('http');
-var operation = require("./operatons");
+//var operation = require("./utils/operatons");
+var { multiplication } = require("./utils/operatons");
 
 var server = http.createServer(function(request, response){
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write("<html><head> <meta charset=\"utf-8\" /> </head> <body>Holá Mundo</body></html>");
     response.end();
 });
-console.log("addition", operation.addition(3,5));
-console.log("multiplication", operation.multiplication(3,5));
+//console.log("addition", operation.addition(3,5));
+//console.log("multiplication", operation.multiplication(3,5));
+console.log("multiplication", multiplication(3,5));
 
 server.listen(5000);

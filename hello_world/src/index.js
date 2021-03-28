f1f3558386c441c6863d3143423052a408063a18
vhavhugo <vhavhugo@gmail.com>
2021-03-28 20:08:13 -0300

    modulos locais

--- hello_world/src/index.js
+++ hello_world/src/index.js
@@ -1,9 +1,12 @@
 var http = require('http');
+var operation = require("./operatons");
 
 var server = http.createServer(function(request, response){
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write("<html><head> <meta charset=\"utf-8\" /> </head> <body>Holá Mundo</body></html>");
     response.end();
 });
+console.log("addition", operation.addition(3,5));
+console.log("multiplication", operation.multiplication(3,5));
 
 server.listen(5000);
---
+++ hello_world/src/operatons.js
@@ -0,0 +1,12 @@
+function addition (num1, num2){
+    return num1 + num2;
+}
+
+function multiplication(num1, num2){
+    return num1 * num2;
+}
+
+module.exports = {
+    addition,
+    multiplication
+}

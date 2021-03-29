3309e8e23097e18fccc1b3995b95065bc48783a3
vhavhugo <vhavhugo@gmail.com>
2021-03-29 07:33:53 -0300

    Rotas express

--- hello_world/src/index.js
+++ hello_world/src/index.js
@@ -6,10 +6,35 @@ const phone = require('phone');
 const PORT = 5000;
 const app = express();
 
-app.listen(PORT, () => {
-    console.log('running on', PORT);
-});
-
 app.get("/", (req, res) => {
     res.status(200).send("<html><head> <meta charset=\"utf-8\" /> </head> <body>Home</body></html>");
+});
+
+app.get("/info", (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).send(JSON.stringify({version: "0.0.1", appName: "Curso Node.js"}));
+});
+
+app.get("/detail", (req, res) => {
+    res.status(200).send("DETAIL");
+});
+
+app.get("/phone", (req, res) => {
+    try {
+        const query = req.query;
+        const {value, country} = req.query;
+        const result = phone(value, country.toUpperCase());
+        res.setHeader("Content-Type", "application/json");
+        res.status(200).send(result);
+    } catch (e) {
+        res.status(400).send(JSON.stringify(e.message));
+    }
+});
+
+app.use((req, res) => {
+    res.status(404).send("NOT FAULT");
+});
+
+app.listen(PORT, () => {
+    console.log('running on', PORT);
 });

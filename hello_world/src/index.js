fc655f6c415307004f4e0da56dd883a00f5d0b53
vhavhugo <vhavhugo@gmail.com>
2021-03-28 23:56:34 -0300

    modulo de terceiros

--- hello_world/src/index.js
+++ hello_world/src/index.js
@@ -1,12 +1,21 @@
 const http = require('http');
+const url = require('url');
 //var operation = require("./utils/operatons");
 const { multiplication } = require("./utils/operatons");
+const phone = require('phone');
 
 const PORT = 5000;
 const apikey = "assasassassasas";
 
 const server = http.createServer((req, res) => {
-    switch(req.url){
+    console.log(req.url);
+    const urlData = url.parse(req.url, true);
+    const path = urlData.pathname;
+    const query = urlData.query;
+    console.log("path",path);
+    console.log("query", query);
+
+    switch(path){
         case "/":
             res.writeHead(200, {"Content-Type": "text/html"});
             res.write("<html><head> <meta charset=\"utf-8\" /> </head> <body>Home</body></html>");  
@@ -18,6 +27,16 @@ const server = http.createServer((req, res) => {
         case "/detail":
             res.writeHead(200, {"Content-Type": "text/html"});
             res.write("<html><head> <meta charset=\"utf-8\" /> </head> <body>Detail</body></html>");  
+            break; 
+        case "/phone":
+            try {
+                const result = phone(query.value, query.country);
+                res.writeHead(200, {"Content-Type": "application/json"});
+                res.write(JSON.stringify(result));  
+            } catch (e) {
+                res.writeHead(400, {"Content-Type": "text/html"});
+                res.write(e.message);
+            }
             break;  
         default:
             res.writeHead(404, {"Content-Type": "text/html"});

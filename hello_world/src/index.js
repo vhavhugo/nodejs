565ef0baa6eaa588e9dc6a18ff7b5dd56eca6fe4
vhavhugo <vhavhugo@gmail.com>
2021-03-28 22:28:46 -0300

    rota switich e application

--- hello_world/src/index.js
+++ hello_world/src/index.js
@@ -5,18 +5,27 @@ const { multiplication } = require("./utils/operatons");
 const PORT = 5000;
 const apikey = "assasassassasas";
 
-const server = http.createServer((request, response) => {
-    if(request.url === "/"){
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.write("<html><head> <meta charset=\"utf-8\" /> </head> <body>Home</body></html>");
-    }else if(request.url === "/info"){
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.write("<html><head> <meta charset=\"utf-8\" /> </head> <body>Info</body></html>");
-    }else{
-        response.writeHead(404, {"Content-Type": "text/html"});
-        response.write("<html><head> <meta charset=\"utf-8\" /> </head> <body>Not found</body></html>");
+const server = http.createServer((req, res) => {
+    switch(req.url){
+        case "/":
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.write("<html><head> <meta charset=\"utf-8\" /> </head> <body>Home</body></html>");  
+            break;
+        case "/info":
+            res.writeHead(200, {"Content-Type": "application/json"});
+            res.write(JSON.stringify({"version": "0.0.1", "appName": "Curso de NodeJS"}));  
+            break;  
+        case "/detail":
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.write("<html><head> <meta charset=\"utf-8\" /> </head> <body>Detail</body></html>");  
+            break;  
+        default:
+            res.writeHead(404, {"Content-Type": "text/html"});
+            res.write("<html><head> <meta charset=\"utf-8\" /> </head> <body>NOT FOUND</body></html>");  
+
     }
-    response.end();
+
+    res.end();
 });
 //console.log("addition", operation.addition(3,5));
 //console.log("multiplication", operation.multiplication(3,5));
